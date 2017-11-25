5683d6dd11a115f018e3b4d7677a1b51c2b14fe5
Ben Bracamonte <ben.bracamonte@gmail.com>
2017-11-24 18:24:44 -0600

    See project button v2

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-(function () {
-  console.log('Make the Visit Project Button work');
-})();
+$(document).ready(function(){
+
+  $('.main-projects-project-thumb').hover(function(event){
+    $(event.target).addClass('main-projects-project-thumb-dark');
+    $('.main-projects-project-button').addClass('main-projects-project-button-show');
+
+    // if ($('.main-projects-project-button:hover')){
+    //   console.log('button hovered');
+    // }
+
+  }, function(event){
+    $(event.target).removeClass('main-projects-project-thumb-dark');
+    $('.main-projects-project-button').removeClass('main-projects-project-button-show');
+  });
+});
