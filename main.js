9eb845da347b75fd3312dcc96f2da0215649e1d9
Ben Bracamonte <ben.bracamonte@gmail.com>
2017-11-25 12:23:31 -0600

    Add social menu hide and show.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+  //Project thumbnail change and see project button on click
   $('.main-projects-project-thumb').hover(function(event){
     $(event.target).addClass('main-projects-project-thumb-dark');
     $('.main-projects-project-button').addClass('main-projects-project-button-show');
@@ -12,4 +13,19 @@ $(document).ready(function(){
     $(event.target).removeClass('main-projects-project-thumb-dark');
     $('.main-projects-project-button').removeClass('main-projects-project-button-show');
   });
+
+  //Header social menu expand on click
+  $('.header-nav-social-title').on('click', function(event){
+    var menu = $('.header-nav-social-menu');
+    if (menu.hasClass('header-nav-social-menu-show')) {
+      menu.removeClass('header-nav-social-menu-show');
+    } else {
+      menu.addClass('header-nav-social-menu-show');
+    }
+  });
+
+
+
+
+
 });
