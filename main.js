b426d0a56103e4c1a47e85bec3d87f05773e373f
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-02-27 16:01:33 -0600

    tie X to close player modal

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
+// jshint asi:true
+
 $(document).ready(function(){
 
   //Project thumbnail change and see project button on hover
   $('.main-projects-project-thumb').hover(function(event){
-    var el = $(event.target);
+    var el = $(event.target)
 
     // Find the right button based on the data-button attribute of the hovered div
-    var button = $('.main-projects-project-button[data-button="'+($(this).data("button"))+'"]') ;
+    var button = $('.main-projects-project-button[data-button="'+($(this).data("button"))+'"]')
 
     // Show the overlay and  button
-    el.addClass('main-projects-project-thumb-dark');
-    button.addClass('main-projects-project-button-show');
+    el.addClass('main-projects-project-thumb-dark')
+    button.addClass('main-projects-project-button-show')
 
   // Callback for hover off, hide the overlay and button
   }, function(event){
@@ -17,20 +19,14 @@ $(document).ready(function(){
      if ($(event.relatedTarget).hasClass('main-projects-project-button')) {
       // do nothing
      } else {
-      $(event.target).removeClass('main-projects-project-thumb-dark');
-      $('.main-projects-project-button').removeClass('main-projects-project-button-show');
+      $(event.target).removeClass('main-projects-project-thumb-dark')
+      $('.main-projects-project-button').removeClass('main-projects-project-button-show')
     }
-  });
+  })
 
-
-  //Header social menu expand on click
-  $('.header-nav-social-title').on('click', function(event){
-    var menu = $('.header-nav-social-menu');
-    if (menu.hasClass('header-nav-social-menu-show')) {
-      menu.removeClass('header-nav-social-menu-show');
-    } else {
-      menu.addClass('header-nav-social-menu-show');
-    }
-  });
-
-});
+  // close player modal on x click
+  $('.modal-player-close-x').on('click', (event) => {
+    $('.modal-player').removeClass('show')
+    $('.overlay').removeClass('show')
+  })
+})
