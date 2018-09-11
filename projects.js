7d3cc15ae1c2df0853f0684e37e2b424c3599f16
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-09-11 15:56:11 -0500

    modal size and miniapp framework

--- projects.js
+++ projects.js
@@ -1,5 +1,14 @@
 // jshint asi:true
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("Javascript working");
+document.addEventListener('DOMContentLoaded', function(event) {
+  const element = document.getElementById('miniappsThumb')
+  element.addEventListener('click', function(event) {
+    const modal = document.getElementById('miniapps')
+    modal.innerHTML = ''
+
+    const node = document.createTextNode('New Text!')
+    const p = document.createElement('p')
+    p.appendChild(node)
+    modal.appendChild(p)
+  })
 })
