8f1257cac462ad1cac55e147213dfce88781841f
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-09-12 12:00:32 -0500

    css button animation

--- projects.js
+++ projects.js
@@ -1,13 +1,36 @@
 // jshint asi:true
 document.addEventListener('DOMContentLoaded', function(event) {
-  const element = document.getElementById('miniappsThumb')
-  element.addEventListener('click', function(event) {
-    const modal = document.getElementById('miniapps')
-    modal.innerHTML = ''
+  const elements = document.querySelectorAll('.project')
 
-    const node = document.createTextNode('Comming Soon!')
-    const p = document.createElement('p')
-    p.appendChild(node)
-    modal.appendChild(p)
+  elements.forEach(element => {
+    const projectButton = element.querySelector('.project-button')
+    const overlay = element.querySelector('.overlay')
+
+    element.addEventListener('mouseover', function(e) {
+      overlay.classList.add('overlay-show')
+      projectButton.classList.add('project-button-show')
+    })
+
+    element.addEventListener('mouseout', function(e) {
+      overlay.classList.remove('overlay-show')
+      projectButton.classList.remove('project-button-show')
+    })
   })
+
+
+  // elements.forEach(e => {
+    // console.log(e)
+  // })
+
+
+  // const element = document.getElementById('miniappsThumb')
+  // element.addEventListener('click', function(event) {
+  //   const modal = document.getElementById('miniapps')
+  //   modal.innerHTML = ''
+
+  //   const node = document.createTextNode('Comming Soon!')
+  //   const p = document.createElement('p')
+  //   p.appendChild(node)
+  //   modal.appendChild(p)
+  // })
 })
