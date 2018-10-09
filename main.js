4939e1105085bbb686b11711610a04c587a713af
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-10-08 21:24:00 -0500

    add sass to resume and hover to icons

--- main.js
+++ main.js
@@ -1,71 +1,15 @@
 // jshint asi:true
-$(document).ready(function(){
+document.addEventListener('DOMContentLoaded', function(event) {
+  const textElements = document.querySelectorAll('.list-inline-text')
 
-  //Project thumbnail change and see project button on hover
-  $('.main-projects-project-thumb').hover(function(event){
-    var el = $(event.target)
 
-    // Find the right button based on the data-button attribute of the hovered div
-    var button = $('.main-projects-project-button[data-button="'+($(this).data("button"))+'"]')
+  textElements.forEach(element => {
+    element.addEventListener('mouseover', function(e) {
+      e.target.classList.add('list-inline-text-show')
+    })
 
-    // Show the overlay and  button
-    el.addClass('main-projects-project-thumb-dark')
-    button.addClass('main-projects-project-button-show')
-
-  // Callback for hover off, hide the overlay and button
-  }, function(event){
-     // If user is hovering on the button element, don't do anything
-     if ($(event.relatedTarget).hasClass('main-projects-project-button')) {
-      // do nothing
-     } else {
-      $(event.target).removeClass('main-projects-project-thumb-dark')
-      $('.main-projects-project-button').removeClass('main-projects-project-button-show')
-    }
+    element.addEventListener('mouseout', function(e) {
+      e.target.classList.remove('list-inline-text-show')
+    })
   })
-
-  // Player Modal
-  $('.player-button').on('click', event => {
-    $('.modal-player-wrapper').addClass('modal-player-show')
-    $('.overlay').addClass('modal-player-show')
-    $('.modal-player-close').addClass('modal-player-show')
-  })
-
-  // close player modal on x click
-  $('.modal-player-close').on('click', event => {
-    $('.modal-player-wrapper').removeClass('modal-player-show')
-    $('.overlay').removeClass('modal-player-show')
-    $('.modal-player-close').removeClass('modal-player-show')
-  })
-
-
-  // Hide player modal on x click
-  // $('.modal-player-close').on('click', event => {
-    // $('.modal-player').removeClass('modal-player-show')
-    // $('.overlay').removeClass('modal-player-show')
-  // })
-
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- projects.js
+++ projects.js
@@ -16,21 +16,4 @@ document.addEventListener('DOMContentLoaded', function(event) {
       projectButton.classList.remove('project-button-show')
     })
   })
-
-
-  // elements.forEach(e => {
-    // console.log(e)
-  // })
-
-
-  // const element = document.getElementById('miniappsThumb')
-  // element.addEventListener('click', function(event) {
-  //   const modal = document.getElementById('miniapps')
-  //   modal.innerHTML = ''
-
-  //   const node = document.createTextNode('Comming Soon!')
-  //   const p = document.createElement('p')
-  //   p.appendChild(node)
-  //   modal.appendChild(p)
-  // })
 })
